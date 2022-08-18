d5ab896e1144359e84adf7185ca07196d5137a27
michelle-ferdinands <86200949+michelle-ferdinands@users.noreply.github.com>
2022-08-18 10:27:45 -0400

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  CS232 Command Shell
+//
+//  Created by Victor Norman on 1/20/15.
+//  Copyright (c) 2015 Victor Norman. All rights reserved.
+//
+
+#include <iostream>
+#include "shell.h"
+#include "path.h"
+
+int main(int argc, const char *argv[])
+{
+    shell myShell;
+    myShell.run();
+}
